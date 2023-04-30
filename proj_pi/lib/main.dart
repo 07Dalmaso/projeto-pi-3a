e8f6e9032d575abc20278ee3d092d94dde3407b7
iLuiszin <oluis10@gmail.com>
2023-04-30 15:39:49 -0300

    feature: adding add card page

--- proj_pi/lib/main.dart
+++ proj_pi/lib/main.dart
@@ -3,13 +3,13 @@
 import 'package:flutter/material.dart';
 import 'package:proj_pi/login_screen.dart';
 //import 'package:proj_pi/user.dart';
-import 'package:proj_pi/cadastro.dart'; 
+import 'package:proj_pi/cadastro.dart';
 import 'package:proj_pi/splash_screen.dart';
 import 'package:proj_pi/test.dart';
 import 'package:proj_pi/profile.dart';
+import 'package:proj_pi/add_card.dart';
 import 'package:proj_pi/gastos.dart';
 import 'dart:ui';
-
 
 /*class _User extends User {
    User minhaInstancia = User();
@@ -22,8 +22,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {   
-
+  Widget build(BuildContext context) {
     final List<Color> colors = [
       Color.fromARGB(255, 69, 72, 73),
       Color.fromARGB(255, 97, 104, 107),
@@ -31,31 +30,31 @@ class MyApp extends StatelessWidget {
       Color.fromARGB(255, 246, 247, 248),
     ];
     return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      primarySwatch: MaterialColor(
-        colors[0].value,
-        <int, Color>{
-          50: colors[0],
-          100: colors[1],
-          200: colors[2],
-          300: colors[3],
-          400: colors[0],
-          500: colors[1],
-          600: colors[2],
-          700: colors[3],
-          800: colors[0],
-          900: colors[1],
-        
-        },
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: MaterialColor(
+          colors[0].value,
+          <int, Color>{
+            50: colors[0],
+            100: colors[1],
+            200: colors[2],
+            300: colors[3],
+            400: colors[0],
+            500: colors[1],
+            600: colors[2],
+            700: colors[3],
+            800: colors[0],
+            900: colors[1],
+          },
+        ),
       ),
-    ),
-     home: Scaffold(
+      home: Scaffold(
         body: CustomScrollView(
           slivers: [
             SliverAppBar(
-              expandedHeight: 250.0, // Define the height of the app bar when it is expanded
+              expandedHeight:
+                  250.0, // Define the height of the app bar when it is expanded
               flexibleSpace: Container(
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
@@ -80,11 +79,12 @@ class MyApp extends StatelessWidget {
       routes: {
         '/main': (context) => const MyHomePage(title: 'Bem vindo(a), Usuário!'),
         '/login': (context) => LoginPage(),
-        '/cadastro': (context) => CadastroPage(), 
-        '/splash_screen':(context) => SplashScreen(),
-        '/test':(context) => TestPage(),
+        '/cadastro': (context) => CadastroPage(),
+        '/splash_screen': (context) => SplashScreen(),
+        '/test': (context) => TestPage(),
         '/profile': (context) => ProfilePage(),
         '/gastos': (context) => GastosPage(),
+        '/add_card': (context) => AddCard()
       },
     );
   }
@@ -109,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Center(
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-           SizedBox(height: 20),
+          SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
               // Código para lidar com o clique no botão
@@ -117,21 +117,19 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Text(
               'Meus Cartões',
               style: TextStyle(fontSize: 20),
-              
             ),
             style: ButtonStyle(
               minimumSize: MaterialStateProperty.all(
                   Size(300, 80)), // Largura de 200 e altura de 50
               backgroundColor: MaterialStateProperty.all(
-                   Color.fromARGB(255, 78, 74, 84)), // Cor de fundo azul
+                  Color.fromARGB(255, 78, 74, 84)), // Cor de fundo azul
             ),
           ),
-           SizedBox(height: 20),
+          SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
               //Navigator.pushNamed(context, '/profile');
             },
-            
             child: Text(
               'Adicionar Cartão',
               style: TextStyle(fontSize: 20),
@@ -140,10 +138,10 @@ class _MyHomePageState extends State<MyHomePage> {
               minimumSize: MaterialStateProperty.all(
                   Size(300, 80)), // Largura de 200 e altura de 50
               backgroundColor: MaterialStateProperty.all(
-                  Color.fromARGB(255,78, 74, 84)), // Cor de fundo azul
+                  Color.fromARGB(255, 78, 74, 84)), // Cor de fundo azul
             ),
           ),
-           SizedBox(height: 20),
+          SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
               Navigator.pushNamed(context, '/gastos');
@@ -151,16 +149,15 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Text(
               'Controle de Gastos',
               style: TextStyle(fontSize: 20),
-
             ),
             style: ButtonStyle(
               minimumSize: MaterialStateProperty.all(
                   Size(300, 80)), // Largura de 200 e altura de 50
               backgroundColor: MaterialStateProperty.all(
-                   Color.fromARGB(255, 78, 74, 84)), // Cor de fundo azul
+                  Color.fromARGB(255, 78, 74, 84)), // Cor de fundo azul
             ),
           ),
-           SizedBox(height: 20),
+          SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
               // Código para lidar com o clique no botão
@@ -168,21 +165,19 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Text(
               'Adicionar de Transações',
               style: TextStyle(fontSize: 20),
-              
             ),
             style: ButtonStyle(
               minimumSize: MaterialStateProperty.all(
                   Size(300, 80)), // Largura de 200 e altura de 50
               backgroundColor: MaterialStateProperty.all(
-                   Color.fromARGB(255, 78, 74, 84)), // Cor de fundo azul
+                  Color.fromARGB(255, 78, 74, 84)), // Cor de fundo azul
             ),
           ),
-           SizedBox(height: 20),
+          SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
               Navigator.pushNamed(context, '/profile');
             },
-            
             child: Text(
               'Meu Perfil',
               style: TextStyle(fontSize: 20),
@@ -191,10 +186,10 @@ class _MyHomePageState extends State<MyHomePage> {
               minimumSize: MaterialStateProperty.all(
                   Size(300, 80)), // Largura de 200 e altura de 50
               backgroundColor: MaterialStateProperty.all(
-                   Color.fromARGB(255, 78, 74, 84)), // Cor de fundo azul
+                  Color.fromARGB(255, 78, 74, 84)), // Cor de fundo azul
             ),
           ),
-           SizedBox(height: 20),
+          SizedBox(height: 20),
         ])));
   }
 }
